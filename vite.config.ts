fdf514ee71a242ad5829de989d0427346c168871
ncattin <164329252+ncattin@users.noreply.github.com>
2026-03-07 19:59:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio-ncattin/",
+  base: "/portfolio-polish/",
   server: {
     host: "::",
     port: 8080,
